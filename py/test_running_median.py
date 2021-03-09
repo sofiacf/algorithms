9cdc63df2da9c4779c791b89ef91019a3a87386e
Sofia Chandler-Freed <dev@smcf.io>
2021-03-08 23:58:07 -0500

    Move running median files out of data structures directory

--- py/test_running_median.py
+++ py/test_running_median.py
@@ -1,12 +1,12 @@
 import unittest
-from py.data_structures.running_median import RunningMedian
+from py.running_median import RunningMedian
 
 
 class TestRunningMedian(unittest.TestCase):
     def test_floats(self):
         total = 0
         running_median = RunningMedian()
-        with open("../../src/running_median/test_data/random_floating_points.txt") as values:
+        with open("../src/running_median/test_data/random_floating_points.txt") as values:
             for value in values:
                 total += running_median.add(float(value))
         self.assertAlmostEqual(4995738.755804, total, 5)
@@ -14,7 +14,7 @@ class TestRunningMedian(unittest.TestCase):
     def test_100_floats(self):
         total = 0
         running_median = RunningMedian(100)
-        with open("../../src/running_median/test_data/random_floating_points.txt") as values:
+        with open("../src/running_median/test_data/random_floating_points.txt") as values:
             for value in values:
                 total += running_median.add(float(value))
         self.assertAlmostEqual(4995205.397700, total, 5)
